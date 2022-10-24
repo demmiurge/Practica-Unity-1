94e0839d1b4ffd0aebffba182ca8377a5ddd1927
GuillemExe <guillem.arrojo@gmail.com>
2022-10-25 00:57:00 +0200

    Updated Inventory Script and Solid Done on DoorController

--- Assets/Code/Entities/Inventory.cs
+++ Assets/Code/Entities/Inventory.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class Inventory : MonoBehaviour
 {
@@ -11,17 +14,51 @@ public class Inventory : MonoBehaviour
         bool l_WasAdded = false;
 
         // Check the existence of the item and if it can be accumulated
-        foreach (CollectibleItem item in m_CollectibleItem)
+        foreach (CollectibleItem l_Item in m_CollectibleItem)
         {
-            if (Item.m_ItemName == item.m_ItemName && Item.m_IsStackable && item.m_IsStackable)
+            if (Item.m_ItemName == l_Item.m_ItemName && Item.m_IsStackable && l_Item.m_IsStackable)
             {
-                item.m_Amount++;
+                l_Item.m_Amount++;
                 l_WasAdded = true;
             }
         }
 
         // If it is not cumulative we add it as something new to the list
         if (l_WasAdded == false) m_CollectibleItem.Add(Item);
+    }
+
+    public bool CheckItemExistsByName(string ItemName)
+    {
+        return m_CollectibleItem.Any(l_Item => ItemName == l_Item.m_ItemName);
+    }
+
+    public bool CheckItemExistsByNameAndQuantity(string ItemName, int ItemAmount)
+    {
+        Debug.Log("TENGO? " + m_CollectibleItem.Count()); 
+
+        foreach (var lItem in m_CollectibleItem)
+        {
+            if (ItemName == lItem.m_ItemName && ItemAmount <= lItem.m_Amount)
+            {
+                Debug.Log("Name " + lItem.m_ItemName);
+                Debug.Log("AMOUNT " + lItem.m_Amount);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void ConsumeItems(string ItemName, int ItemAmount)
+    {
+        for (int i = 0; i < m_CollectibleItem.Count; i++)
+        {
+            if (ItemName == m_CollectibleItem[i].m_ItemName && ItemAmount <= m_CollectibleItem[i].m_Amount)
+                m_CollectibleItem[i].m_Amount -= ItemAmount;
+
+            if (m_CollectibleItem[i].m_Amount == 0) 
+                m_CollectibleItem.RemoveAt(i);
+        }
     }
 
     // Start is called before the first frame update
---
+++ Assets/Code/Utilities/DoorController.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public enum OpeningForms
+{
+    Distance,
+    Points,
+    Keys,
+    Keyboard,
+}
+
+public class DoorController : MonoBehaviour
+{
+    bool m_IsOpen;
+    GameObject m_Player;
+
+    public Animation m_AnimationDoor;
+    public AnimationClip m_AnimationNameToOpenDoor;
+    public AnimationClip m_AnimationNameToCloseDoor;
+
+    public OpeningForms m_OpeningForm;
+    public OpeningForms m_ChangeWayToOpen;
+
+    public float m_ActivationDistance = 3;
+    public float m_PointsRequired = 1000;
+    public string m_NameOfKeyItem;
+    public int m_NumberOfKeysRequired = 1;
+
+    public KeyCode m_KeyToOpen = KeyCode.O;
+    public KeyCode m_KeyToClose = KeyCode.C;
+    public KeyCode m_KeyToInteract = KeyCode.E;
+
+    public void GateState(bool Open = true)
+    {
+        if (!m_AnimationDoor.isPlaying && m_IsOpen != Open)
+        {
+            m_AnimationDoor.Play(Open ? m_AnimationNameToOpenDoor.name : m_AnimationNameToCloseDoor.name);
+            m_IsOpen = Open;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_IsOpen = false;
+        m_Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (m_OpeningForm)
+        {
+            case OpeningForms.Distance:
+                OpenByDistance();
+                break;
+            case OpeningForms.Points:
+                OpenByPoints();
+                break;
+            case OpeningForms.Keys:
+                OpenByKey();
+                break;
+            case OpeningForms.Keyboard:
+                OpenByKeyboard();
+                break;
+        }
+    }
+
+    void OpenByDistance()
+    {
+        if (InDistance()) GateState();
+        else GateState(false);
+    }
+
+    void OpenByPoints()
+    {
+        OpenByKeyboard();
+    }
+
+    void OpenByKey()
+    {
+        if (InDistance())
+        {
+            Inventory l_Inventory = m_Player.GetComponent<Inventory>();
+
+            if (l_Inventory.CheckItemExistsByNameAndQuantity(m_NameOfKeyItem, m_NumberOfKeysRequired))
+            {
+                l_Inventory.ConsumeItems(m_NameOfKeyItem, m_NumberOfKeysRequired);
+                m_OpeningForm = m_ChangeWayToOpen;
+            }
+        }
+    }
+
+    void OpenByKeyboard()
+    {
+        if (Input.GetKeyDown(m_KeyToClose)) GateState(false);
+        if (Input.GetKeyDown(m_KeyToOpen)) GateState();
+        if (Input.GetKeyDown(m_KeyToInteract)) GateState(!m_IsOpen);
+    }
+
+    private bool InDistance()
+    {
+        return (transform.position - m_Player.transform.position).magnitude <= m_ActivationDistance;
+    }
+}
