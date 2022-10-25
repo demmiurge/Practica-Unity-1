ee9392361c89c4cdd4f797850cd1a34d63d890f7
Veronika Bolshakova <veronikabox@gmail.com>
2022-10-25 14:38:22 +0200

    Level one update

--- Assets/Code/Player/ShootingGallery.cs
+++ Assets/Code/Player/ShootingGallery.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ShootingGallery : MonoBehaviour
 {
+    public Canvas m_PlayerHud;
+    public TMP_Text m_TextMeshPro;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +22,14 @@ public class ShootingGallery : MonoBehaviour
 
     public void ActivateShootingGallery()
     {
+        //Debug.Log("Shooting Gallery");
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            m_TextMeshPro.gameObject.SetActive(true);
+        }
     }
 }
